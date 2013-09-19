cfc08f43d3777dd5d4341e04c59645ad7c033f72
eri451 <eri@c3d2.de>
2013-09-19 17:03:02 +0200

    hello I'm eri

--- scripts/newsig.sh
+++ scripts/newsig.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #inspired by: http://www.guckes.net/Setup/zshrc.mine
-KEYID=0x9E949439
+KEYID=0x65F349F2
 gpg --refresh-keys $KEYID 2>/dev/null
 gpg --recv-keys `gpg --list-sigs $KEYID|grep "not found"|cut -b 9-17`
 gpg --list-sigs $KEYID | sed 's/^......................//' | sort -n | tail $@
